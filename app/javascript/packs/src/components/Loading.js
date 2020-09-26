ad97f55de77db87b09014c6deb95ea58f930a98d
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-26 17:38:03 +0300

    Add Loading component

---
+++ app/javascript/packs/src/components/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/loading.css';
+
+const Loading = () => (
+  <div className="sk-chase">
+    <div className="sk-chase-dot" />
+    <div className="sk-chase-dot" />
+    <div className="sk-chase-dot" />
+    <div className="sk-chase-dot" />
+    <div className="sk-chase-dot" />
+    <div className="sk-chase-dot" />
+  </div>
+);
+
+export default Loading;
