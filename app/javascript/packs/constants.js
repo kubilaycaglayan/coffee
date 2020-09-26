633bb25261138f072430880bf7ddc329878184b7
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-26 18:02:32 +0300

    Change URL bases

--- app/javascript/packs/constants.js
+++ app/javascript/packs/constants.js
@@ -9,24 +9,25 @@ export const DESTROY_SESSION = 'DESTROY_SESSION';
 export const RECORD_COFFEES = 'RECORD_COFFEES';
 export const RECORD_COFFEE = 'RECORD_COFFEE';
 
-export const LOGIN_URL = 'http://localhost:3000/sessions/create/';
-export const LOGOUT_URL = 'http://localhost:3000/sessions/destroy/';
-export const GET_COFFEES_URL = 'http://localhost:3000/api/coffees';
-export const GET_COFFEE_URL = 'http://localhost:3000/api/coffees/';
+export const LOGIN_URL = '/sessions/create/';
+export const LOGOUT_URL = '/sessions/destroy/';
+export const GET_COFFEES_URL = '/api/coffees';
+export const GET_COFFEE_URL = '/api/coffees/';
 
 export const DEFAULT_SESSION = {
   loggedIn: false,
   email: '',
 };
-export const DEFAULT_COFFEES = [{}];
 export const DEFAULT_COFFEE = {
   id: 0,
   name: '',
   description: '',
   photo: '',
 };
+export const DEFAULT_COFFEES = [DEFAULT_COFFEE];
 
 export const INITIAL_STATE = {
   session: DEFAULT_SESSION,
-  coffees: [],
+  coffees: DEFAULT_COFFEES,
+  coffee: DEFAULT_COFFEE,
 };
