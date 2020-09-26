a8bfd6c5719d7ed28d429a5ff2cbc140e591a06e
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-26 13:00:36 +0300

    Add redux-devtools-extension

--- app/javascript/packs/src/app/Provided.js
+++ app/javascript/packs/src/app/Provided.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from '../reducers';
 import App from './App';
 import { INITIAL_STATE } from '../../constants';
 
-const store = createStore(reducer, INITIAL_STATE, applyMiddleware(thunk));
+const store = createStore(reducer, INITIAL_STATE, composeWithDevTools(applyMiddleware(thunk)));
 
 const Provided = () => (
   <Provider store={store}>
