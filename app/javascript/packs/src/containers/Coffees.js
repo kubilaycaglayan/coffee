845608d3aa4d6eeb6b1607982314134e1518ee0a
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-27 17:53:09 +0300

    Add card style

--- app/javascript/packs/src/containers/Coffees.js
+++ app/javascript/packs/src/containers/Coffees.js
@@ -20,7 +20,7 @@ const Coffees = props => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
+    arrows: false,
   };
 
   return (
@@ -33,16 +33,29 @@ const Coffees = props => {
               <h1>
                 Coffees
               </h1>
-              <Slider {...settings}>
+              <Slider {...settings} className="mt-5 shadow">
                 {
                   coffees.map(coffee => (
-                    <div key={coffee.id}>
-                      <img src={coffee.photo} alt="bean" />
-                      <h2>
-                        <Link to={`/coffee/${coffee.id}`}>
-                          {coffee.name}
+                    <div className="card px-4 border-0 d-flex flex-column justify-content-between" key={coffee.id}>
+                      <img className="card-img-top" src={coffee.photo} alt="coffee representation" />
+                      <div className="card-body d-flex flex-column justify-content-between">
+                        <div className="stars">
+                          <h5 className="card-title">
+                            {coffee.name}
+                          </h5>
+                          <i className="fas fa-star" />
+                          <i className="fas fa-star" />
+                          <i className="fas fa-star" />
+                          <i className="fas fa-star" />
+                          <i className="far fa-star" />
+                        </div>
+                        <p className="card-text">
+                          {coffee.description}
+                        </p>
+                        <Link to={`/coffee/${coffee.id}`} className="mt-3 text-right align-self-end">
+                          Details
                         </Link>
-                      </h2>
+                      </div>
                     </div>
                   ))
                 }
