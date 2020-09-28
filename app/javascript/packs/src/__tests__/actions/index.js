3b6605800bce6e79c2ba18f742d8ffe205b15a37
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-28 13:12:29 +0300

    Opt out for compile test packs to a separate directory

--- app/javascript/packs/src/__tests__/actions/index.js
+++ app/javascript/packs/src/__tests__/actions/index.js
@@ -1,23 +1,17 @@
 import {
-  ATTEMPT,
-  LOADING,
-  CREATE_SESSION,
-  DESTROY_SESSION,
-  RECORD_COFFEES,
-  RECORD_COFFEE,
-  RECORD_FAVORITES,
-  CHANGE_NEW_COFFEE,
   CHANGE_FLASH,
 } from '../../../constants';
 import * as action from '../../actions';
 
 describe('actions', () => {
-  test('description', () => {
-    const filter = '99';
-    const expectedAction = {
-      type: CHANGE_FILTER,
-      filter,
-    };
-    expect(action.changeFilter(filter)).toEqual(expectedAction);
+  describe('returns an action object message and the type CHANGE_FLASH', () => {
+    test(CHANGE_FLASH, () => {
+      const message = 'Success';
+      const expectedAction = {
+        type: CHANGE_FLASH,
+        message,
+      };
+      expect(action.changeFlash(message)).toEqual(expectedAction);
+    });
   });
 });
