aebc577f623efa54c724d0b21aac557a8087c6a3
kubilaycaglayan <kblycaglayan@gmail.com>
2020-10-03 23:35:25 +0300

    Add tests for email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe User, type: :model do
     )
   end
 
+  let(:invalid_user) { User.new(email: 'something', password: '000000', password_confirmation: '000000') }
+  let(:invalid_user2) { User.new(email: 't@t.com', password: '000000', password_confirmation: '000000') }
+  let(:valid_user) { User.new(email: 'something@something.com', password: '000000', password_confirmation: '000000') }
+
   describe 'associations' do
     it 'has many favorites' do
       expect(user).to respond_to(:favorites)
@@ -20,6 +24,21 @@ RSpec.describe User, type: :model do
 
     it 'falsy' do
       expect(user).not_to respond_to(:favs)
+    end
+  end
+
+  describe 'validations' do
+    it 'validates the type of email' do
+      expect(invalid_user).not_to be_valid
+    end
+
+    it 'validates the type of email' do
+      expect(valid_user).to be_valid
+    end
+
+    it 'validates the uniqueness of email' do
+      p User.create(email: 't@t.com', password: '000000', password_confirmation: '000000')
+      expect(invalid_user2).not_to be_valid
     end
   end
 end
