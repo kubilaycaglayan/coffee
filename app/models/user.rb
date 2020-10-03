38a30a0ee4c1bcac70cfd7452733cbcf19c07638
kubilaycaglayan <kblycaglayan@gmail.com>
2020-10-03 19:19:16 +0300

    Add email validation and uniuqeness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :favorites, dependent: :destroy
   has_many :favorite_coffees, source: :coffee, through: :favorites, class_name: 'Coffee'
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /.+@.+/, message: 'Email invalid' }
 end
